26dfd644205c683980570ebcbdc5b2a6b988e330
Marsel <t.mars@mail.ru>
2017-02-10 09:04:09 +0300

    fix

--- src/com/talipov/Consumer.java
+++ src/com/talipov/Consumer.java
@@ -4,7 +4,7 @@ package com.talipov;
  * Created by Марсель on 10.02.2017.
  */
 public class Consumer {
-    private int value;
+    private volatile int value;
     private Sem s3 = new Sem("s3", 1);
     private Sem s2 = new Sem("s2", 1);
     private Sem s1 = new Sem("s1", 1);
